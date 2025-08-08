6920366f3f224ac28e8f8708a0eb04220a9078aa
Julius Kakai <cjkakai5@gmail.com>
2025-08-08 22:09:00 +0300

    add match functionality

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,9 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+    
+    def match(self, word_list):
+        return [w for w in word_list
+                if w.lower() != self.word
+                and sorted(w.lower()) == sorted(self.word)]
+        
